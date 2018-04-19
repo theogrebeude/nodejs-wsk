37649222c275b6910bd1b0bd0b64499d287b8964
Théo Grébeude <theogrebeude@gmail.com>
2018-04-19 14:42:42 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
-function greet(args) {
-  return { msg: `Hello ${args.name}!`}
+
+function main(args) {
+	if (!${args}){
+		var json = ${args.page}!;
+		var title = JSON.parse(title);
+		var date = Date.now();
+		return { title: title, date: date}
+
+	}else{
+		return { title: `null`, date: `null}
+	}
 }
+
+
 
